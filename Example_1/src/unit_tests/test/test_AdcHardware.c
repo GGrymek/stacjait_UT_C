ceda047e3e74ed20f3b75c7e0f4f78803369520f
GrzegorzG <Grzegorz.Grymek@biocam.pl>
2023-01-13 21:28:49 +0100

    Stacja.it UT C environments.

---
+++ Example_1/src/unit_tests/test/test_AdcHardware.c
@@ -0,0 +1,34 @@
+/* Must have includes */
+/* Include unity.h */
+#include "unity.h"
+
+/* Include basic types, specific types configurations etc... */
+
+/* Include header of Unit Under Test */
+#include "AdcHardware.h"
+
+/* Include all required mock headers */
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_FunctionName_TestName_1(void)
+{
+}
+
+void test_FunctionName_TestName_2(void)
+{
+}
+
+void test_FunctionName_TestName_3(void)
+{
+}
+
+void test_FunctionName_TestName_4(void)
+{
+}
