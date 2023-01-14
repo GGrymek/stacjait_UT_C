d26b745bf4ea67bdf76b8dce58e511c659cb2d10
GrzegorzG <Grzegorz.Grymek@biocam.pl>
2023-01-14 14:25:27 +0100

    Test environment makefile for example 1 case 2.

---
+++ Example_1/src/unit_test_1/test/test_UsartConductor.c
@@ -0,0 +1,38 @@
+/* Must have includes */
+/* Include unity.h */
+#include "unity.h"
+
+/* Include basic types, specific types configurations etc... */
+#include "Types.h"
+
+/* Include header of Unit Under Test (UUT) */
+#include "UsartConductor.h"
+
+/* Include all required mock headers */
+#include "mock_UsartHardware.h"
+#include "mock_UsartModel.h"
+#include "mock_TaskScheduler.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_FunctionName_TestCaseName_1(void)
+{
+}
+
+void test_FunctionName_TestName_2(void)
+{
+}
+
+void test_FunctionName_TestName_3(void)
+{
+}
+
+void test_FunctionName_TestName_4(void)
+{
+}
